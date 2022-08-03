b9378c012f4276ecaa00637b0b5b34237577b027
Zenckoo <109071153+Zenckoo@users.noreply.github.com>
2022-08-03 18:45:43 +0200

    Add files via upload

---
+++ ex03/ft_recursive_power.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_power.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: isitbon <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/01 18:58:53 by isitbon           #+#    #+#             */
+/*   Updated: 2022/08/01 19:37:25 by isitbon          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_recursive_power(int nb, int power)
+{
+	if (power < 0)
+	{
+		return (0);
+	}
+	if (power == 0)
+	{
+		return (1);
+	}
+	if (power > 1)
+	{
+		return (nb * ft_recursive_power(nb, power - 1));
+	}
+	return (nb);
+}
